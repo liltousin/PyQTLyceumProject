20886d88112f671267f023789ea225d7b01a486d
liltousin <sergeypletnev2004@gmail.com>
2023-02-11 18:28:49 +0300

    add func common to main and akk_info

--- akk_status_funcs.py
+++ akk_status_funcs.py
@@ -2,8 +2,10 @@ import os
 
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
 from telethon.sync import TelegramClient
+from sqlite3 import Connection
 
 from tg_auth_funcions import get_api_id_api_hash
+from sql_functions import set_akk_status
 
 
 def check_nofile_status(phone: str):
@@ -42,8 +44,18 @@ def check_ban_status(phone: str):
     return status
 
 
-# def check_akk_and_update(phone: str)
+def check_akk_and_update(phone: str, db_status: str, connection: Connection):
+    try:
+        if db_status == 'banned':
+            status = check_ban_status(phone)
+        else:
+            status = check_akk_status(phone)
+        set_akk_status(connection, status, phone)
+        return status
+    except ConnectionError:
+        return 'Нет подключения к интернету!'
 
-if __name__ == '__main__':
-    phone = '6283134468453'
-    print(check_akk_status(phone))
+
+# if __name__ == '__main__':
+#     phone = '6283134468453'
+#     print(check_akk_status(phone))
